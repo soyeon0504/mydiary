4fe94686b98fecfe6d1bf876fc6182f76bb5adda
soyeon <dev.vsoyeon@gmail.com>
2023-11-29 17:08:13 +0900

    feat : 기본 레이아웃 구성 진행

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { BotBar } from '../styles/footerstyle';
+const Footer = () => {
+
+  return (
+    <BotBar>홍길동</BotBar>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { TopBar } from '../styles/headerstyle';
+const Header = () => {
+
+  return(
+      <TopBar>
+        <button>
+          <img src="images/bt_list.svg" />
+        </button>
+        <h2>타이틀</h2>
+        <button>
+          <img src="images/bt_login.svg" />
+        </button>
+    </TopBar>
+)
+};
+
+export default Header
